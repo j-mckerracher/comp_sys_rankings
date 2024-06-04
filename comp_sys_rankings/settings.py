8b09ce22b098cf805ce3e61197b3e343cfba2c2b
Josh <jmckerra@purdue.edu>
2024-06-03 20:27:39 -0600

    getting ready for vercel

--- comp_sys_rankings/settings.py
+++ comp_sys_rankings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's)rvh-%5v0$xro-5-zb&5v%%euh25b$pk$m6164a9h#%rlyd*!'
+SECRET_KEY = os.getenv('django_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
