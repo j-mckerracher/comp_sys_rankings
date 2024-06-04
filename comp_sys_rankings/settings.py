b29036f3cf761862c8559f33d4a29c59057cb169
Josh <jmckerra@purdue.edu>
2024-06-04 10:13:00 -0600

    getting ready for vercel

--- comp_sys_rankings/settings.py
+++ comp_sys_rankings/settings.py
@@ -69,17 +69,17 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'comp_sys_rankings.wsgi.application'
+WSGI_APPLICATION = 'comp_sys_rankings.wsgi.app'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
