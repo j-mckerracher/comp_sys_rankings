b7ffc7267a0f44c565413ba981509a802a971697
Josh <jmckerra@purdue.edu>
2024-06-04 16:19:44 -0600

    update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Updating and installing python"
+sudo apt update
+sudo apt install python3
 echo "Using Python version:"
 pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
